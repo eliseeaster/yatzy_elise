9788371d2adf8a76a4a9572cfb0b3d72ab4d5ccc
Elise Easter <56083509+eliseeaster@users.noreply.github.com>
2020-09-08 08:58:00 +0200

    Initial commit

---
+++ src/main/java/no/kristiania/YatzyCategory.java
@@ -0,0 +1,5 @@
+package no.kristiania;
+
+public enum YatzyCategory {
+    ONES
+}
---
+++ src/main/java/no/kristiania/YatzyGame.java
@@ -0,0 +1,7 @@
+package no.kristiania;
+
+public class YatzyGame {
+    public int score(YatzyCategory category, int[] dice) {
+        return 0;
+    }
+}
